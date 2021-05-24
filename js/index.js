360e3ac068a9d1ea2e37a18e751bfbb3e3aeb942
Leonardo Cardozo <jcardozo@gruposabra.com>
2021-05-24 16:37:03 -0300

    cambios visual del portfolio

--- js/index.js
+++ js/index.js
@@ -1,2 +1,52 @@
-document.getElementById('fomulario')
-    .addEventListener('click',(e) => e.preventDefault() )
+gify = document.querySelector('#gify');
+consultorio = document.querySelector('#consultorio');
+gara = document.querySelector('#gara');
+weather = document.querySelector('#weather');
+
+
+window.addEventListener('scroll', () => {
+
+  var posiciones = {
+    gify : gify.getBoundingClientRect().top,
+    consultorio : consultorio.getBoundingClientRect().top,
+    gara : gara.getBoundingClientRect().top,
+    weather : weather.getBoundingClientRect().top
+  };
+
+  last_known_scroll_position = window.scrollY;
+
+  botonTop = document.getElementById('boton-top');
+  
+  if (last_known_scroll_position == 0) {
+    botonTop.style.display = "none";
+  }
+  else{
+    botonTop.style.display = "";
+  }
+  
+  let windowHeight = window.innerHeight;
+
+  if(posiciones['gify'] < windowHeight){
+    gify.style.animation = 'mover 1s ease-out';
+  }
+  else 
+  {
+    gify.style.animation = 'none';
+  }
+
+  if(posiciones['consultorio'] < windowHeight)
+    consultorio.style.animation = 'mover1 1s ease-out';
+  else 
+    consultorio.style.animation = 'none';
+
+  if(posiciones['gara'] < windowHeight){
+    gara.style.animation = 'mover 1s ease-out';
+  }
+  else gara.style.animation = 'none';
+
+  if(posiciones['weather'] < windowHeight){
+    weather.style.animation = 'mover1 1s ease-out';
+  }
+  else weather.style.animation = 'none';
+
+});
