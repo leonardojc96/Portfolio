13e81ae992bd27017f4ae5b825d5635d1b3a42c9
Leonardo Cardozo <jcardozo@gruposabra.com>
2021-05-24 23:40:58 -0300

    agregado contactos

--- js/index.js
+++ js/index.js
@@ -1,27 +1,17 @@
-gify = document.querySelector('#gify');
-consultorio = document.querySelector('#consultorio');
-gara = document.querySelector('#gara');
-weather = document.querySelector('#weather');
 navbar = document.querySelector('#navbar');
+copied = document.querySelector('#copied');
 body = document.querySelector('body');
+botonTop = document.getElementById('boton-top');
+mail = 'leonardojc96@gmail.com';
 
 window.onload = () => {
   navbar.style.display = 'none';
+  botonTop.style.display = 'none';
+  copy.style.display = 'none'
 }
 
 window.addEventListener('scroll', () => {
-
-
-  var posiciones = {
-    gify : gify.getBoundingClientRect().top,
-    consultorio : consultorio.getBoundingClientRect().top,
-    gara : gara.getBoundingClientRect().top,
-    weather : weather.getBoundingClientRect().top
-  };
-
   last_known_scroll_position = window.scrollY;
-
-  botonTop = document.getElementById('boton-top');
   
   if (last_known_scroll_position == 0) {
     botonTop.style.display = "none";
@@ -31,30 +21,20 @@ window.addEventListener('scroll', () => {
     botonTop.style.display = "";
     navbar.style.display = '';
   }
-  
-  let windowHeight = window.innerHeight;
-
-  if(posiciones['gify'] < windowHeight){
-    gify.style.animation = 'mover 1s ease-out';
-  }
-  else 
-  {
-    gify.style.animation = 'none';
-  }
-
-  if(posiciones['consultorio'] < windowHeight)
-    consultorio.style.animation = 'mover1 1s ease-out';
-  else 
-    consultorio.style.animation = 'none';
-
-  if(posiciones['gara'] < windowHeight){
-    gara.style.animation = 'mover 1s ease-out';
-  }
-  else gara.style.animation = 'none';
-
-  if(posiciones['weather'] < windowHeight){
-    weather.style.animation = 'mover1 1s ease-out';
-  }
-  else weather.style.animation = 'none';
 
 });
+
+function copy(){
+  copyToClipboar(mail);
+}
+
+function copyToClipboar(text){
+  navigator.clipboard.writeText(text).then(function() {
+    console.log('Async: Copying to clipboard was successful!', text);
+    var popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+  }, function(err) {
+    console.error('Async: Could not copy text: ', err);
+  });
+}
+
